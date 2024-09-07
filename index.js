9e3258df710baa153fee00ffb807086d52d7341c
abdelrhmanali1812 <abdelrhmanali1812@gmail.com>
2024-09-07 12:30:42 +0300

    upload files

--- index.js
+++ index.js
@@ -25,10 +25,11 @@ dbConnections();
 /* Global */
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
 app.use(cors());
-app.set('views', path.join(__dirname, 'views')); // Adjust the path as necessary
 app.set('views', path.join(__dirname, 'views'));
 
 /* Session */
