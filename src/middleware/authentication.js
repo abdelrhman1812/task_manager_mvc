cbce1dff58330df39e3663351b9ee279cfc15118
abdelrhmanali1812 <abdelrhmanali1812@gmail.com>
2024-09-13 19:33:03 +0300

    update Validation

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -3,15 +3,10 @@ import UserModel from "../../database/models/user.model.js";
 const auth = async (req, res, next) => {
     const userId = req.session?.user?.id;
 
-    if (!userId) {
+    if (!req.session?.user) {
         req.flash("error", 'Invalid session. Please log in again.');
         return res.redirect('/auth/login');
     }
-
-
-
-
-
 
     const userExist = await UserModel.findById({ _id: req.session?.user?.id });
 
--- src/models/auth/auth.validation.js
+++ src/models/auth/auth.validation.js
@@ -4,11 +4,14 @@ const registerValidation = Joi.object({
     name: Joi.string().min(3).max(12).required(),
     email: Joi.string().email().required(),
     password: Joi.string().pattern(/^[A-Z][A-Za-z0-9]{3,40}$/)
+        .message('must start with an uppercase letter and be between 4 and 40 characters long like A123456')
 })
 
 const loginValidation = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().pattern(/^[A-Z][A-Za-z0-9]{3,40}$/)
+        .message('must start with an uppercase letter and be between 4 and 40 characters long like A123456')
+
 })
 
 
